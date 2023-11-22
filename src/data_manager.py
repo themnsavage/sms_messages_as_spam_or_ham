e813bbaaf7566b629a4aebe747eb12dfd21099b6
themnsavage <mnsavage@crimson.ua.edu>
2023-11-22 16:53:12 -0600

    feat(data_manager): split data into training and test sets

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,15 +1,37 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 class Data_Manager:
     def __init__(self, file):
         self._data_set = self._read_data_set_from_file(file)
-        
+        (
+            self._feature_train,
+            self._feature_test,
+            self._label_train,
+            self._label_test,
+        ) = self._split_data_into_training_and_testing_sets()
+
     def _read_data_set_from_file(self, file):
         # Replace 'path_to_your_data_file' with the actual file path
-        return pd.read_csv(file, sep='\t', header=None, names=['Label', 'Message'])
-    
+        return pd.read_csv(file, sep="\t", header=None, names=["Label", "Message"])
+
     def get_data_set(self):
         return self._data_set
-    
+
     def get_data_set_head(self):
         return self._data_set.head()
+
+    def _split_data_into_training_and_testing_sets(self):
+        return train_test_split(
+            self._data_set["Message"],
+            self._data_set["Label"],
+            test_size=0.2,
+            random_state=42,
+        )
+
+    def get_train_data_set(self):
+        return self._feature_train, self._label_train
+
+    def get_test_data_set(self):
+        return self._feature_test, self._label_test
--- src/main.py
+++ src/main.py
@@ -4,7 +4,18 @@ from data_manager import Data_Manager
 def main():
     file = "SMSSpamCollection"
     data_manager = Data_Manager(file=file)
-    print(data_manager.get_data_set_head())
+    print(f"data set head:\n{data_manager.get_data_set_head()}")
+    print("\n\n\n")
+
+    feature_train, label_train = data_manager.get_train_data_set()
+    print(f"data set train feature:\n {feature_train.head()}\n")
+    print(f"data set train label:\n {label_train.head()}")
+    print("\n\n\n")
+
+    feature_test, label_test = data_manager.get_test_data_set()
+    print(f"data set test feature:\n {feature_test.head()}\n")
+    print(f"data set test label:\n {label_test.head()}")
+    print("\n\n\n")
 
 
 if __name__ == "__main__":
