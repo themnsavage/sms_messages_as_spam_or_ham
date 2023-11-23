d92287fbcae85d84034e09134d8d14fcca24ce06
themnsavage <60998598+themnsavage@users.noreply.github.com>
2023-11-23 01:23:14 -0600

    feat: create more features and got a good model (#1)

--- src/main.py
+++ src/main.py
@@ -5,14 +5,15 @@ def main():
     file = "SMSSpamCollection"
     model_manager = Model_Manager(file=file)
 
-    # model = model_manager.make_model_with_training_data(max_models=4)
+    # model = model_manager.make_model_with_training_data(max_models=35)
     # model_manager.save_model(model=model, path="model")
     # print(model_manager.evaluate_performance(model=model))
-    
-    model = model_manager.use_saved_model(path='model/GLM_1_AutoML_1_20231122_221751')
-    # print(model_manager.evaluate_performance(model))
-    # print(model_manager.make_predictions(model=model))
-    model_manager.make_single_prediction(model=model)
+    # model_manager.print_pearson_correlation()
+
+    model = model_manager.use_saved_model(path='model/GBM_3_AutoML_1_20231123_10521')
+    print(model_manager.evaluate_performance(model=model))
+    print(model_manager.make_predictions(model=model))
+    print(model_manager.make_single_prediction(model=model,single_message="Free entry in 2 a wkly comp to win FA Cup final tkts 21st May 2005. Text FA to 87121 to receive entry question(std txt rate)T&C's apply 08452810075over18's"))
 
 if __name__ == "__main__":
     main()
--- src/model_manager.py
+++ src/model_manager.py
@@ -1,23 +1,15 @@
 import pandas as pd
 import re
-import nltk
 import h2o
-import joblib
-from sklearn.feature_extraction.text import TfidfVectorizer
+from textblob import TextBlob
 from sklearn.model_selection import train_test_split
 from h2o.automl import H2OAutoML
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
 
-# Download necessary NLTK datasets
-nltk.download('punkt')
-nltk.download('stopwords')
 
 class Model_Manager:
     def __init__(self, file=None):
         self._data_set = (
-            self._read_data_set_from_file(file) if file is not None else None
+            self._process_data_set_from_file(file) if file is not None else None
         )
         if file is not None:
             (
@@ -26,11 +18,80 @@ class Model_Manager:
                 self._label_train,
                 self._label_test,
             ) = self._split_data_into_training_and_testing_sets()
+        
+        self._chosen_features = [
+            "Message",
+            "Message_Length",
+            "Uppercase_Count",
+            "Count_Punctuations",
+            "URL_Count",
+            "Digit_Count",
+        ]
 
-    def _read_data_set_from_file(self, file):
+    def _process_data_set_from_file(self, file):
         df = pd.read_csv(file, sep="\t", header=None, names=["Label", "Message"])
-        df["Tokens"] = df["Message"].apply(self._filter_message)
+
+        df = self._create_more_features(df=df)
+
         return df
+
+    def _create_more_features(self, df):
+        df["Message_Length"] = df["Message"].apply(self._message_length)
+        df["Word_Count"] = df["Message"].apply(self._count_of_words)
+        df["Character_Count"] = df["Message"].apply(self._count_of_characters)
+        df["Uppercase_Count"] = df["Message"].apply(self._count_uppercase_words)
+        df["Count_Punctuations"] = df["Message"].apply(self._count_punctuation)
+        df["URL_Count"] = df["Message"].apply(self._count_urls)
+        df["Digit_Count"] = df["Message"].apply(self._count_digits)
+        df["Special_Char_Count"] = df["Message"].apply(self._count_special_characters)
+        df["Sentiment_Score"] = df["Message"].apply(self._sentiment_score)
+        
+        return df
+    
+    def _message_length(self, message):
+        return len(message)
+
+    def _count_of_words(self, message):
+        return len(message.split())
+
+    def _count_of_characters(self, message):
+        return sum(len(word) for word in message.split())
+
+    def _count_punctuation(self, message, punctuation="!?"):
+        return sum(message.count(char) for char in punctuation)
+
+    def _count_uppercase_words(self, message):
+        return sum(word.isupper() for word in message.split())
+
+    def _count_urls(self, message):
+        url_pattern = r"https?://\S+|www\.\S+"
+        return len(re.findall(url_pattern, message))
+
+    def _count_digits(self, message):
+        return sum(char.isdigit() for char in message)
+
+    def _count_special_characters(self, message, characters="@#$%&"):
+        return sum(message.count(char) for char in characters)
+
+    def _sentiment_score(self, message):
+        return TextBlob(message).sentiment.polarity
+
+    def print_pearson_correlation(self):
+        df = self._data_set
+
+        label_map = {"spam": 1, "ham": 0}
+        df["Label"] = df["Label"].map(label_map)
+
+        correlations = df.corr()
+        target_correlations = correlations["Label"].sort_values(ascending=False)
+
+        print(target_correlations)
+        
+        features = df[['Character_Count', 'Message_Length', 'Word_Count']]
+        correlation_matrix = features.corr()
+
+        print('multicollinearity:\n')
+        print(correlation_matrix)
 
     def get_data_set(self):
         return self._data_set
@@ -39,8 +100,19 @@ class Model_Manager:
         return self._data_set.head()
 
     def _split_data_into_training_and_testing_sets(self):
+        feature_columns = [
+            "Message",
+            "Message_Length",
+            "Word_Count",
+            "Character_Count",
+            "Uppercase_Count",
+            "Count_Punctuations",
+            "URL_Count",
+            "Digit_Count",
+            "Sentiment_Score",
+        ]
         return train_test_split(
-            self._data_set["Tokens"],
+            self._data_set[feature_columns],
             self._data_set["Label"],
             test_size=0.2,
             random_state=42,
@@ -61,27 +133,23 @@ class Model_Manager:
     ):
         data_feature = self._feature_train if data_feature is None else data_feature
         data_label = self._label_train if data_label is None else data_label
-        # Tokenize and transform the feature data
-        tfidf_vectorizer = TfidfVectorizer()
-        tfidf_features = tfidf_vectorizer.fit_transform(data_feature)
-
-        joblib.dump(tfidf_vectorizer, 'fitted_tfidf_vectorizer.pkl')    
-        # Convert to DataFrame
-        tfidf_df = pd.DataFrame(tfidf_features.toarray(), columns=tfidf_vectorizer.get_feature_names_out())
-
-        # Concatenate with the label
-        data_for_h2o = pd.concat([tfidf_df, data_label.reset_index(drop=True)], axis=1)
 
         h2o.init()
 
-        # Create H2OFrame
-        h2o_train = h2o.H2OFrame(data_for_h2o)
+        h2o_train = h2o.H2OFrame(pd.concat([data_feature, data_label], axis=1))
 
-        feature = tfidf_vectorizer.get_feature_names_out().tolist()
+        aml = H2OAutoML(
+            max_models=max_models,
+            seed=1,
+            balance_classes=True,
+            max_runtime_secs=max_runtime_secs,
+            include_algos=["GBM", "XGBoost", "DeepLearning"],
+        )
+
         target = "Label"
 
-        aml = H2OAutoML(max_models=max_models, seed=1, balance_classes=True, max_runtime_secs=max_runtime_secs)
-        aml.train(x=feature, y=target, training_frame=h2o_train)
+        aml = H2OAutoML(max_models=10, seed=1)
+        aml.train(x=self._chosen_features, y=target, training_frame=h2o_train)
 
         return aml.leader
 
@@ -95,24 +163,14 @@ class Model_Manager:
         return predictions
 
     def make_single_prediction(self, model, single_message="hello this is a message"):
-        tfidf_vectorizer = joblib.load('fitted_tfidf_vectorizer.pkl')
-        # Preprocess the message
-        # (You should implement the _filter_message method similar to how you preprocessed your training data)
-        processed_message = self._filter_message(single_message)
+        df_single = pd.DataFrame([single_message], columns=["Message"])
+        df_single = self._create_more_features(df=df_single)
+        
+        h2o_single_message = h2o.H2OFrame(df_single)
 
-        # Transform the message using the loaded TF-IDF vectorizer
-        tfidf_features = tfidf_vectorizer.transform([processed_message])
+        prediction = model.predict(h2o_single_message)
 
-        # Convert to H2OFrame
-        h2o_message = h2o.H2OFrame(pd.DataFrame(tfidf_features.toarray()))
-
-        # Make prediction
-        prediction = model.predict(h2o_message)
-
-        # Convert the prediction to a readable format
-        predicted_label = prediction.as_data_frame().iloc[0, 0]
-
-        return predicted_label
+        return prediction.as_data_frame().iloc[0, 0]
 
     def evaluate_performance(self, model, data_feature=None, data_label=None):
         data_feature = self._feature_test if data_feature is None else data_feature
@@ -126,26 +184,3 @@ class Model_Manager:
         model_path = h2o.save_model(model=model, path=path, force=True)
         print("Model saved to: " + model_path)
         return model_path
-    
-
-
-    def _filter_message(self, message):
-        # Convert to lowercase
-        message = message.lower()
-
-        # Remove punctuation and non-word characters
-        message = re.sub(r'[^\w\s]', '', message)
-
-        # Tokenize the message
-        tokens = word_tokenize(message)
-
-        # Optionally: Remove stop words
-        stop_words = set(stopwords.words('english'))
-        tokens = [word for word in tokens if word not in stop_words]
-
-        # Optionally: Apply stemming
-        stemmer = PorterStemmer()
-        tokens = [stemmer.stem(word) for word in tokens]
-
-        return ' '.join(tokens)
-
